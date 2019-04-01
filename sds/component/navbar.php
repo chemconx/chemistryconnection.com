6303ad632491180006a19d72f7a38801d4ffed8c
Jasper <DrOverbuild@users.noreply.github.com>
2019-04-01 17:01:10 -0500

    Add links to homepage in navbar

--- sds/component/navbar.php
+++ sds/component/navbar.php
@@ -1,6 +1,6 @@
 <div class="navbar">
 	<div class="main main-header">
-		<img class="header-image" src="../img/chemconx.png">
-		<h1>Safety Data Sheets</h1>
+		<a href="/sds/"><img class="header-image" src="../img/chemconx.png"></a>
+		<h1><a href="/sds/">Safety Data Sheets</a></h1>
 	</div>
 </div>
